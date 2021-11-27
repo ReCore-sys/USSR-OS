bb78337a4fa5be606ede52846239f834cac99dd1
ReCore-sys <shor0124@asms.sa.edu.au>
2021-11-27 16:17:06 +1030

    ?

--- server.lua
+++ server.lua
@@ -1,1 +1,19 @@
-print("Testing")
+component = require("component")
+function getid()
+    return component.list()
+    
+end
+
+print("Initialising...")
+json = require "json"
+idfile = io.open("ids.json", "r")
+if idfile then
+    ids = json.decode(idfile:read("*all"))
+    idfile:close()
+else
+    ids = {}
+    idfile:close()
+end
+
+print(getid())
+
