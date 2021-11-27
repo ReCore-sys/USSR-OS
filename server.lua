59c9d75e986839217a168cc52872e84f88be8517
ReCore-sys <shor0124@asms.sa.edu.au>
2021-11-27 16:41:42 +1030

    ?

--- server.lua
+++ server.lua
@@ -7,17 +7,21 @@ end
 function file_exists(name)
     local f=io.open(name,"r")
     if f~=nil then io.close(f) return true else return false end
- end
+end
 
 print("Initialising...")
 json = require "json"
 if file_exists("ids.json") then
     IDFILE = io.open("ids.json", "r")
-    ids = json.decode(IDFILE:read("*all"))
+    ids = json.decode(IDFILE:read("a"))
     IDFILE:close()
 else
     ids = {}
 end
+print("Initialised")
+ownid = getid()
 
-print(getid())
+for k,v in pairs(ownid) do
+    print(k,v)
+   end
 
