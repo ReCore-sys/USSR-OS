d1bed01759c511d903c2e36cfa6ccd7c3e8fc2df
ReCore-sys <shor0124@asms.sa.edu.au>
2021-11-27 16:34:14 +1030

    ?

--- server.lua
+++ server.lua
@@ -1,13 +1,17 @@
 component = require("component")
 function getid()
     return component.list()
-    
 end
+
+function file_exists(name)
+    local f=io.open(name,"r")
+    if f~=nil then io.close(f) return true else return false end
+ end
 
 print("Initialising...")
 json = require "json"
-IDFILE = io.open("ids.json", "r")
-if IDFILE then
+if file_exists(ids.json) then
+    IDFILE = io.open("ids.json", "r")
     ids = json.decode(IDFILE:read("*all"))
     IDFILE:close()
 else
